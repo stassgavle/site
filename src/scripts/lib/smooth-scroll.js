08c50ad572a454874f88a2311b797a5eb2513929
Robin Andersson <k.robin.andersson@gmail.com>
2019-06-13 15:26:06 +0200

    use Prettier to format code
    Commit includes reformat of existing files

--- src/scripts/lib/smooth-scroll.js
+++ src/scripts/lib/smooth-scroll.js
@@ -1,5 +1,4 @@
 function scrollIt(destination, duration = 200, easing = 'linear', callback) {
-
   const easings = {
     linear(t) {
       return t;
@@ -17,7 +16,7 @@ function scrollIt(destination, duration = 200, easing = 'linear', callback) {
       return t * t * t;
     },
     easeOutCubic(t) {
-      return (--t) * t * t + 1;
+      return --t * t * t + 1;
     },
     easeInOutCubic(t) {
       return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
@@ -26,30 +25,45 @@ function scrollIt(destination, duration = 200, easing = 'linear', callback) {
       return t * t * t * t;
     },
     easeOutQuart(t) {
-      return 1 - (--t) * t * t * t;
+      return 1 - --t * t * t * t;
     },
     easeInOutQuart(t) {
-      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
     },
     easeInQuint(t) {
       return t * t * t * t * t;
     },
     easeOutQuint(t) {
-      return 1 + (--t) * t * t * t * t;
+      return 1 + --t * t * t * t * t;
     },
     easeInOutQuint(t) {
-      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-    }
+      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+    },
   };
 
   const start = window.pageYOffset;
-  const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+  const startTime =
+    'now' in window.performance ? performance.now() : new Date().getTime();
 
-  const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+  const documentHeight = Math.max(
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight
+  );
+  const windowHeight =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.getElementsByTagName('body')[0].clientHeight;
   console.log(destination);
-  const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-  const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+  const destinationOffset =
+    typeof destination === 'number' ? destination : destination.offsetTop;
+  const destinationOffsetToScroll = Math.round(
+    documentHeight - destinationOffset < windowHeight
+      ? documentHeight - windowHeight
+      : destinationOffset
+  );
 
   if ('requestAnimationFrame' in window === false) {
     window.scroll(0, destinationOffsetToScroll);
@@ -60,10 +74,14 @@ function scrollIt(destination, duration = 200, easing = 'linear', callback) {
   }
 
   function scroll() {
-    const now = 'now' in window.performance ? performance.now() : new Date().getTime();
-    const time = Math.min(1, ((now - startTime) / duration));
+    const now =
+      'now' in window.performance ? performance.now() : new Date().getTime();
+    const time = Math.min(1, (now - startTime) / duration);
     const timeFunction = easings[easing](time);
-    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+    window.scroll(
+      0,
+      Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start)
+    );
 
     if (window.pageYOffset === destinationOffsetToScroll) {
       if (callback) {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,19 @@
 var setupLinkScrollHandlers = function() {
   document.getElementById('brands-link').addEventListener('click', () => {
-    scrollIt(
-      document.getElementById('brands'),
-      1200,
-      'easeOutQuad',
-      () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+    scrollIt(document.getElementById('brands'), 1200, 'easeOutQuad', () =>
+      console.log(`Just finished scrolling to ${window.pageYOffset}px`)
     );
   });
-  document.getElementById('opening-hours-link').addEventListener('click', () => {
-    scrollIt(
-      document.getElementById('opening-hours'),
-      1600,
-      'easeOutQuad',
-      () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
-    );
-  });
+  document
+    .getElementById('opening-hours-link')
+    .addEventListener('click', () => {
+      scrollIt(
+        document.getElementById('opening-hours'),
+        1600,
+        'easeOutQuad',
+        () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+      );
+    });
   document.getElementById('map-link').addEventListener('click', () => {
     scrollIt(
       document.getElementById('map').offsetTop + 500,
@@ -24,14 +23,11 @@ var setupLinkScrollHandlers = function() {
     );
   });
   document.getElementById('contact-link').addEventListener('click', () => {
-    scrollIt(
-      document.getElementById('contact'),
-      1600,
-      'easeOutQuad',
-      () => console.log(`Just finished scrolling to ${window.pageYOffset}px`)
+    scrollIt(document.getElementById('contact'), 1600, 'easeOutQuad', () =>
+      console.log(`Just finished scrolling to ${window.pageYOffset}px`)
     );
   });
-}
+};
 
 var setupMobileMenuCloseHandlers = function() {
   var elements = document.getElementsByClassName('menu-link');
@@ -41,18 +37,17 @@ var setupMobileMenuCloseHandlers = function() {
       document.getElementById('menu-toggle-checkbox').checked = false;
     });
   }
-}
+};
 
 var writeCopyrightYear = function() {
   var currentYear = new Date().getFullYear();
   if (currentYear > 2018) {
     document.getElementById('js-copy-year').innerHTML = '2018 - ' + currentYear;
   }
-}
+};
 
 window.onload = function() {
   setupLinkScrollHandlers();
   setupMobileMenuCloseHandlers();
   writeCopyrightYear();
 };
-
